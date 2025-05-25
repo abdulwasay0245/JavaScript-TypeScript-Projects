82da73dd2ee0a48d592ee624916f63a3a151ef2c
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-25 17:42:06 +0500

    added age_calculator

---
+++ age_calculator/app.js
@@ -0,0 +1,30 @@
+// const date = new Date()
+// const currentYear = date.getFullYear()
+// console.log(`Current Year is ${currentYear}`)
+
+// process.stdin.on('data', (data) => {
+//     const userYear = data;
+//     const result = currentYear - userYear 
+//     console.log(`Your Age is ${result}`);
+//     process.exit()
+    
+// })
+
+// function wasay() {
+//     let i = 1
+//     console.log(`I Love You ${i}`);
+//     i = 1+
+//     wasay();
+    
+// }
+// wasay()
+
+// do {
+//     console.log('wasay');
+    
+// } while ([] && '');
+
+while (payment = true) {
+    console.log("Your can proceed");
+    
+}
