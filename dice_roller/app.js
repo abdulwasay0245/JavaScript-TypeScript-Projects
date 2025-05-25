591d9ecc4764f3aa8ec14e91b6c1a7c27c47b232
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-25 23:50:11 +0500

    added some functionality in dice

--- dice_roller/app.js
+++ dice_roller/app.js
@@ -1,3 +1,14 @@
-const randomNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+// const randomNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1
 
-console.log(`You got a ${randomNumber}`)
+// console.log(`You got a ${randomNumber}`)
+
+
+const btn = document.getElementById("rollBtn")
+function diceRoll() {
+    const dice = document.getElementById("dice")
+    const resultDiv = document.createElement("div")
+    resultDiv.classList.add("resultDiv")
+    document.querySelector("main").appendChild(resultDiv)
+}
+
+btn.addEventListener('click', diceRoll)
