97754f9c3315202f59b358fe288e848e23c81fdb
Abdul Wasay <abdulwasay0245@gmail.com>
2025-05-26 08:41:05 +0500

    completed dice logics

--- dice_roller/app.js
+++ dice_roller/app.js
@@ -4,9 +4,31 @@
 
 let elementNumber = 1
 const btn = document.getElementById("rollBtn")
+const dataSection = document.getElementById("data")
+const dice = document.getElementById("dice")
+
+
+function getDiceFace(rollResult) {
+    switch (rollResult) {
+      case 1:
+        return "&#9856;";
+      case 2:
+        return "&#9857;";
+      case 3:
+        return "&#9858;";
+      case 4:
+        return "&#9859;";
+      case 5:
+        return "&#9860;";
+      case 6:
+        return "&#9861;";
+      default:
+        return "";
+    }
+  }
+
+
 function diceRoll() {
-    const dataSection = document.getElementById("data")
-    const dice = document.getElementById("dice")
     dice.innerHTML = ""
     const randomNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1
     dice.classList.add("roll")
@@ -22,11 +44,16 @@ function diceRoll() {
     
     const dataElements = document.createElement("div")
     const heading = document.createElement("h1")
+    const smallDice = document.createElement("span")
     dataElements.classList.add("dataElement")
     dataSection.appendChild(dataElements)
-    dataElements.appendChild(heading)   
+    dataElements.appendChild(heading)
+    dataElements.appendChild(smallDice)
+    smallDice.classList.add("smallDice")
+    smallDice.innerHTML = getDiceFace(randomNumber)
     heading.innerText = elementNumber;
     elementNumber = elementNumber + 1
+   
 }
 
 
